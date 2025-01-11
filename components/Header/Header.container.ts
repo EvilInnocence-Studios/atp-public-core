e7d1c08c8cc3ed9df73a802298751dc1f14a045d
Andrew Wittrock <andy@wittrock.us>
2025-01-10 21:51:47 -0600

    Link search term to query parameters

--- components/Header/Header.container.ts
+++ components/Header/Header.container.ts
@@ -1,14 +1,24 @@
 import { createInjector, inject, mergeProps } from "unstateless";
 import {HeaderComponent} from "./Header.component";
 import {IHeaderInputProps, HeaderProps, IHeaderProps} from "./Header.d";
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router";
 
 const injectHeaderProps = createInjector(({}:IHeaderInputProps):IHeaderProps => {
-    const [search, setSearch] = useState('');
+    const [query, setQuery] = useSearchParams();
+    const [search, setSearch] = useState(query.get('q') || '');
+
     const navigate = useNavigate();
 
+    useEffect(() => {
+        setSearch(query.get('q') || '');
+    }, [query.get('q')])
+
     const runSearch = () => {
+        setQuery(q => {
+            q.set('q', search);
+            return q;
+        });
         navigate(`/products?q=${search}`);
     }
 
