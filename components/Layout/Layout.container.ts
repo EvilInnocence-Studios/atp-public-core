d4f9f5a2a08f8017fba8fe3326dc7a7960881b31
Andrew Wittrock <andy@wittrock.us>
2025-02-07 15:09:32 -0600

    Add Google analytics

--- components/Layout/Layout.container.ts
+++ components/Layout/Layout.container.ts
@@ -1,8 +1,10 @@
 import { createInjector, inject, mergeProps } from "unstateless";
 import {LayoutComponent} from "./Layout.component";
 import {ILayoutInputProps, LayoutProps, ILayoutProps} from "./Layout.d";
+import usePageTracking from "@core/lib/usePageTracking";
 
 const injectLayoutProps = createInjector(({}:ILayoutInputProps):ILayoutProps => {
+    usePageTracking();
     return {};
 });
 
