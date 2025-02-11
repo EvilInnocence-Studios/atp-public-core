c07f08390b4179703245c5241f912cf66270803e
Andrew Wittrock <andy@wittrock.us>
2025-02-11 00:02:37 -0600

    Sort homepage banners

--- components/HomePage/HomePage.component.tsx
+++ components/HomePage/HomePage.component.tsx
@@ -1,16 +1,19 @@
 import { Banner } from "@common/components/Banner";
 import { NewProducts } from "@store/components/NewProducts";
 import { Col, Row, Spin } from "antd";
+import { prop, sort } from "ts-functional";
 import { HomePageProps } from "./HomePage.d";
 import styles from './HomePage.module.scss';
+
+const sortBanners = sort.by(prop<any, any>("order")).asc;
 
 export const HomePageComponent = ({largeBanners, smallBanners, isLoading}:HomePageProps) =>
     <Spin spinning={isLoading}>
             <Row className={styles.homepage} gutter={16}>
-            {largeBanners.map(banner => <Col lg={12} sm={24} key={banner.id} className={styles.hero}>
+            {largeBanners.sort(sortBanners).map(banner => <Col lg={12} sm={24} key={banner.id} className={styles.hero}>
                 <Banner banner={banner} />
             </Col>)}
-            {smallBanners.map(banner => <Col lg={8} sm={24} key={banner.id} className={styles.hero}>
+            {smallBanners.sort(sortBanners).map(banner => <Col lg={8} sm={24} key={banner.id} className={styles.hero}>
                 <Banner banner={banner} />
             </Col>)}
             <Col xs={24}>
