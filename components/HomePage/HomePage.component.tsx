29d0cd8f4e3ea9d2a9ff1d6eb03cb2d1c86d7b46
Andrew Wittrock <andy@wittrock.us>
2025-02-11 20:18:57 -0600

    Sort homepage banners

--- components/HomePage/HomePage.component.tsx
+++ components/HomePage/HomePage.component.tsx
@@ -1,21 +1,40 @@
 import { Banner } from "@common/components/Banner";
 import { NewProducts } from "@store/components/NewProducts";
 import { Col, Row, Spin } from "antd";
-import { prop, sort } from "ts-functional";
+import { objMap, pipe, /*prop, sort*/ } from "ts-functional";
 import { HomePageProps } from "./HomePage.d";
 import styles from './HomePage.module.scss';
+import { IBanner } from "@common-shared/banner/types";
+import { Index } from "ts-functional/dist/types";
+import { Fader } from "@core/components/Fader";
 
-const sortBanners = sort.by(prop<any, any>("order")).asc;
+// const sortBanners = sort.by(prop<any, any>("order")).asc;
+
+// Group banners by order value
+const groupBanners = (banners: IBanner[]):Index<IBanner[]> => banners.reduce(
+    (acc:Index<IBanner[]>, banner:IBanner) => {
+        const key = `${banner.order}`;
+        return {...acc, [key]: [...acc[key] || [], banner]};
+    },
+    {}
+);
+
+const renderBanners = (lg:number) => (banners:IBanner[], order:string) => <Col lg={lg} sm={24} key={order} className={styles.hero}>
+    <Fader interval={5}>
+        {banners.map(banner => <Banner key={banner.id} banner={banner} />)}
+    </Fader>
+</Col>;
+
+const valuesByOrder = (elements:Index<JSX.Element>) => Object.keys(elements).sort().map(key => elements[key]);
+
+const mapBanners = (lg:number) => pipe(groupBanners, objMap(renderBanners(lg)), valuesByOrder);
 
 export const HomePageComponent = ({largeBanners, smallBanners, isLoading}:HomePageProps) =>
     <Spin spinning={isLoading}>
-            <Row className={styles.homepage} gutter={16}>
-            {largeBanners.sort(sortBanners).map(banner => <Col lg={12} sm={24} key={banner.id} className={styles.hero}>
-                <Banner banner={banner} />
-            </Col>)}
-            {smallBanners.sort(sortBanners).map(banner => <Col lg={8} sm={24} key={banner.id} className={styles.hero}>
-                <Banner banner={banner} />
-            </Col>)}
+        <Row className={styles.homepage} gutter={16}>
+            {mapBanners(12)(largeBanners)}
+            {mapBanners(8)(smallBanners)}
+
             <Col xs={24}>
                 <NewProducts />
             </Col>
--- components/Layout/Layout.component.tsx
+++ components/Layout/Layout.component.tsx
@@ -9,6 +9,11 @@ import styles from './Layout.module.scss';
 export const LayoutComponent = ({}:LayoutProps) =>
     <Layout>
         <Layout.Header>
+            {/* This is a hidden input field to stop the browser from autofilling the search field in the header*/}
+            <div style={{width: 0, position: "absolute", right: 999999, top: 0}}>
+                <input />
+                <input type="password" />
+            </div>
             <Header />
         </Layout.Header>
         <Layout.Content className={styles.content}>
