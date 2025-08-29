c0e5d009c4967e0cd69379be794b3f5d5d8f8c17
Andrew Wittrock <andy@wittrock.us>
2025-08-28 21:50:03 -0500

    Remove hard-coded pages

--- routes.tsx
+++ routes.tsx
@@ -1,9 +1,7 @@
 import { HomePage } from "@public/components/HomePage";
-import { ContactUs } from "./components/ContactUs";
 
 export const publicRoutes = {
     public: [
-        {path: "/",                                component: HomePage                   },
-        {path: "/contacts",                        component: ContactUs                  },
+        {path: "/", component: HomePage},
     ]
 }
