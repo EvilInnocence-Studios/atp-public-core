1c49be82de811f38632e91aaa626e20216dd959b
Andrew Wittrock <andy@wittrock.us>
2025-09-26 00:46:37 -0500

    Move sitePlugins to common module

--- index.ts
+++ index.ts
@@ -1,12 +1,7 @@
 import { IModule } from "@core/lib/module";
 import { publicRoutes } from "./routes";
-import { rendererPlugins } from "@core/lib/plugins/renderer";
 
 export const module:IModule = {
     name: "public",
     routes: publicRoutes,
-};
-
-export const sitePlugins = {
-    homepage: rendererPlugins(),
 };
